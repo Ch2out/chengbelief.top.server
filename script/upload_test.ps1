8c56cb1b52c44e38441e70b47757fa7952491fc6
chengbelief <2629091913@qq.com>
2022-12-25 15:17:20 +0800

    message项目上传

--- script/upload_test.ps1
+++ script/upload_test.ps1
@@ -3,4 +3,5 @@ $env:Path = "C:\Program Files\PuTTY\;"+$env:Path
 $puttyConfigDir = "D:\git-project\chengbelief.top.server\key\private.ppk"
 $serverDir = "chenghan@120.24.189.233:/home/chenghan/javaweb/"
 $warFile = "D:\project\war\book.war"
+$warFile = "D:\project\war\message.war"
 pscp -i $puttyConfigDir $warFile $serverDir
