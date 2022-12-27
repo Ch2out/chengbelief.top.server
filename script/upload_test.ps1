37ea9dba2f4c84da39c63f72d90866140ae0052b
chengbelief <2629091913@qq.com>
2022-12-27 21:27:34 +0800

    完成修改

--- script/upload_test.ps1
+++ script/upload_test.ps1
@@ -2,6 +2,7 @@
 $env:Path = "C:\Program Files\PuTTY\;"+$env:Path
 $puttyConfigDir = "D:\git-project\chengbelief.top.server\key\private.ppk"
 $serverDir = "chenghan@120.24.189.233:/home/chenghan/javaweb/"
-$warFile = "D:\project\war\book.war"
-$warFile = "D:\project\war\message.war"
-pscp -i $puttyConfigDir $warFile $serverDir
+$warFilea = "D:\project\war\book.war"
+# $warFileb = "D:\project\war\message.war"
+pscp -i $puttyConfigDir $warFilea $serverDir
+# pscp -i $puttyConfigDir $warFileb $serverDir
